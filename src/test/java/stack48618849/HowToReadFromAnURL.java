81b0c1a5817dcb4e260c539ee711e59b83a60582
Jan Schnasse <schnasse@gmx.de>
2018-02-07 14:08:08 +0100

    add redirects and argument map

--- src/test/java/stack48618849/HowToReadFromAnURL.java
+++ src/test/java/stack48618849/HowToReadFromAnURL.java
@@ -1,18 +1,18 @@
-/*******************************************************************************
- * Copyright 2018 Jan Schnasse
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
- ******************************************************************************/
+    /*******************************************************************************
+     * Copyright 2018 Jan Schnasse
+     * 
+     * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+     * use this file except in compliance with the License.  You may obtain a copy
+     * of the License at
+     * 
+     *   http://www.apache.org/licenses/LICENSE-2.0
+     * 
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+     * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+     * License for the specific language governing permissions and limitations under
+     * the License.
+     ******************************************************************************/
 
     package stack48618849;
 
@@ -20,12 +20,16 @@
     import java.io.IOException;
     import java.io.InputStream;
     import java.net.HttpURLConnection;
+    import java.net.MalformedURLException;
     import java.net.URL;
     import java.nio.charset.StandardCharsets;
     import java.util.Base64;
-    
+    import java.util.HashMap;
+    import java.util.Map;
+    import java.util.Map.Entry;
+
     import org.junit.Test;
-    
+
     public class HowToReadFromAnURL {
     	@Test
     	public void readFromUrl() {
@@ -35,35 +39,28 @@
     			throw new RuntimeException(e);
     		}
     	}
-    	
+
     	@Test(expected = RuntimeException.class)
     	public void readFromUrlWithBasicAuth() {
-    		String user="aUser";
-    		String passwd="aPasswd";
-    		try (InputStream in = getInputStreamFromUrl("https://jira.atlassian.com/rest/api/2/issue/JSWCLOUD-11658",user,passwd)) {
+    		String user = "aUser";
+    		String passwd = "aPasswd";
+    		try (InputStream in = getInputStreamFromUrl(
+    						new URL("https://jira.atlassian.com/rest/api/2/issue/JSWCLOUD-11658"), user, passwd)) {
     			System.out.println(convertInputStreamToString(in));
     		} catch (Exception e) {
-    			System.out.println("If basic auth is provided, it should be correct: "+e.getMessage());
+    			System.out.println("If basic auth is provided, it should be correct: " + e.getMessage());
     			throw new RuntimeException(e);
     		}
     	}
 
-    	private InputStream getInputStreamFromUrl(String urlString,String user, String passwd) throws IOException {
-    		URL url = new URL(urlString);
-    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    		conn.setRequestMethod("GET");
-    		conn.setRequestProperty("Accept", "application/json");
-    		String encoded = Base64.getEncoder().encodeToString((user+":"+passwd).getBytes(StandardCharsets.UTF_8));  
-    		conn.setRequestProperty("Authorization", "Basic "+encoded);
-    		return conn.getInputStream();
+    	private InputStream getInputStreamFromUrl(URL url, String user, String passwd) throws IOException {
+    		String encoded = Base64.getEncoder().encodeToString((user + ":" + passwd).getBytes(StandardCharsets.UTF_8));
+    		return urlToInputStream(url, mapOf("Accept", "application/json", "Authorization", "Basic " + encoded,
+    						"User-Agent", "myApplication"));
     	}
-    	
-    	private InputStream getInputStreamFromUrl(String urlString) throws IOException {
-    		URL url = new URL(urlString);
-    		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    		conn.setRequestMethod("GET");
-    		conn.setRequestProperty("Accept", "application/json");
-    		return conn.getInputStream();
+
+    	private InputStream getInputStreamFromUrl(String url) throws IOException {
+    		return urlToInputStream(new URL(url), mapOf("Accept", "application/json", "User-Agent", "myApplication"));
     	}
 
     	private String convertInputStreamToString(InputStream inputStream) throws IOException {
@@ -75,4 +72,49 @@
     		}
     		return result.toString("UTF-8");
     	}
+
+    	private InputStream urlToInputStream(URL url, Map<String, String> args) {
+    		HttpURLConnection con = null;
+    		InputStream inputStream = null;
+    		try {
+    			con = (HttpURLConnection) url.openConnection();
+    			con.setConnectTimeout(15000);
+    			con.setReadTimeout(15000);
+    			for (Entry<String, String> e : args.entrySet()) {
+    				con.setRequestProperty(e.getKey(), e.getValue());
+    			}
+    			con.connect();
+    			int responseCode = con.getResponseCode();
+    			if (responseCode == HttpURLConnection.HTTP_MOVED_PERM || responseCode == HttpURLConnection.HTTP_MOVED_TEMP
+    							|| responseCode == 307 || responseCode == 303) {
+    				String redirectUrl = con.getHeaderField("Location");
+    				try {
+    					URL newUrl = new URL(redirectUrl);
+    					return urlToInputStream(newUrl, args);
+    				} catch (MalformedURLException e) {
+    					URL newUrl = new URL(url.getProtocol() + "://" + url.getHost() + redirectUrl);
+    					return urlToInputStream(newUrl, args);
+    				}
+    			}
+    			inputStream = con.getInputStream();
+    			return inputStream;
+    		} catch (Exception e) {
+    			throw new RuntimeException(e);
+    		}
+    	}
+
+    	private <K, V> Map<K, V> mapOf(Object... keyValues) {
+    		Map<K, V> map = new HashMap<>();
+    		K key = null;
+    		for (int index = 0; index < keyValues.length; index++) {
+    			if (index % 2 == 0) {
+    				key = (K) keyValues[index];
+    			} else {
+    				map.put(key, (V) keyValues[index]);
+    			}
+    		}
+
+    		return map;
+    	}
+
     }

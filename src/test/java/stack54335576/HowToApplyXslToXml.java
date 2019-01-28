dd8b58f36496bbae2fbd12a30bcb03f9a23ca1d5
Jan Schnasse <jan@schnasse.eu>
2019-01-28 09:08:30 +0100

    add answer to https://stackoverflow.com/questions/54335576

---
+++ src/test/java/stack54335576/HowToApplyXslToXml.java
@@ -0,0 +1,67 @@
+package stack54335576;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.SourceLocator;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class HowToApplyXslToXml {
+
+	@Test
+	public void applyMultipleXsl() {
+		InputStream xml = Thread.currentThread().getContextClassLoader().getResourceAsStream("stack54335576/repo.xml");
+		InputStream xsl = Thread.currentThread().getContextClassLoader().getResourceAsStream("stack54335576/join.xsl");
+		String booksXmlPath = "src/test/java/stack54335576/books.xml";
+		String articlesXmlPath = "src/test/java/stack54335576/articles.xml";
+		Document xslDocument = readXml(xsl);
+		insertFileSourceIntoXsl(xslDocument,"bookFile", booksXmlPath);
+		insertFileSourceIntoXsl(xslDocument,"articleFile", articlesXmlPath);
+		xsl(xml, new DOMSource(xslDocument));
+	}
+
+	private void insertFileSourceIntoXsl(Document xslDocument, String name,String xmlPath) {
+		Element param = xslDocument.createElement("xsl:param");
+		param.setAttribute("name", name);
+		param.setAttribute("select", "document('"+xmlPath+"')");
+		xslDocument.getFirstChild().appendChild(param);
+	}
+
+	public void xsl(InputStream xml, Source xsl) {
+		try {
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Templates template = factory.newTemplates(xsl);
+			Transformer xformer = template.newTransformer();
+			Source source = new StreamSource(xml);
+			Result result = new StreamResult(System.out);
+			xformer.transform(source, result);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Document readXml(InputStream xmlin) {
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			return db.parse(xmlin);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/test/java/stack54335576/join.xsl
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+	
+
+	<!-- The java app will dynamically source in files like: <xsl:param name="bookFile" 
+		select="document('src/test/java/stack54335576/books.xml')"/> <xsl:param name="articleFile" 
+		select="document('src/test/java/stack54335576/articles.xml')"/> -->
+	
+
+	<xsl:output method="xml" encoding="UTF-8" indent="yes" />
+
+	<xsl:template match="/">
+		<titleStatusJoin>
+			<xsl:for-each select="//book">
+				<xsl:variable name="myId" select="id" />
+				<book>
+					<status>
+						<xsl:value-of select="status" />
+					</status>
+					<title>
+						<xsl:for-each select="$bookFile//book">
+							<xsl:variable name="bookId" select="@id" />
+							<xsl:choose>
+								<xsl:when test="$myId = $bookId">
+									<xsl:value-of select="title" />
+								</xsl:when>
+							</xsl:choose>
+						</xsl:for-each>
+					</title>
+				</book>
+			</xsl:for-each>
+			<xsl:for-each select="//article">
+				<xsl:variable name="myId" select="id" />
+				<article>
+					<status>
+						<xsl:value-of select="status" />
+					</status>
+					<title>
+						<xsl:for-each select="$articleFile//article">
+							<xsl:variable name="bookId" select="@id" />
+							<xsl:choose>
+								<xsl:when test="$myId = $bookId">
+									<xsl:value-of select="title" />
+								</xsl:when>
+							</xsl:choose>
+						</xsl:for-each>
+					</title>
+				</article>
+			</xsl:for-each>
+
+		</titleStatusJoin>
+	</xsl:template>
+</xsl:stylesheet>
